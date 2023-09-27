6021b131ea01d436f9336764a1db2909f6a54b77
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-27 12:31:40 -0300

    add sideBar.jsx and menu.jsx

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,12 +1,13 @@
 import '../common/template/dependencies'
 import React from "react";
 import Header from '../common/template/header';
+import Sidebar from '../common/template/sideBar';
 
 export default props =>(
     <div className="wrapper">
-        <Header>
+        <Header />
+        <Sidebar/>
         
-        </Header>
       
     </div>
 )
