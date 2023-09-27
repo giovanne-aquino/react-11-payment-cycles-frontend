501bb37424062416dc6f8a930625ea2d64cedc57
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-27 11:57:30 -0300

    fix errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { ReactDOM } from "react";
-import app from "./main/app";
+import  ReactDOM  from "react-dom";
+import App from "./main/app";
 
-ReactDOM.render(<app/>, document.getElementById('app'))
+
+ReactDOM.render(<App />, document.getElementById('app'))
