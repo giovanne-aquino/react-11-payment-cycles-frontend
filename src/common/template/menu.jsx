f7213db806e5641f20f1afccee22bf48b5c7cee5
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-27 12:39:35 -0300

    finalizes basic visual configuration of the menu

--- src/common/template/menu.jsx
+++ src/common/template/menu.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import MenuItem from './menuItem'
+import MenuTree from './menuTree'
 
 export default props => (
     <ul className='sidebar-menu'>
-       
+        <MenuItem path ='#' label='Dashboard' icon='dashboard'/>
+        <MenuTree label= 'Login' icon ='edit'>
+            <MenuItem path ='#billingCycles'
+                label='payment cycle' icon = 'usd' />
+
+        </MenuTree>
     </ul>
 )
