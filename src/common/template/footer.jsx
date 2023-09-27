57d25979bfda94fe59f42baa052a072644bd5c8b
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-27 12:44:39 -0300

    add footer.jsx

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2017
+            <a href='https://github.com/giovanne-aquino-clear' target='_blank'> Giovanne B Aquino</a>.
+        </strong>
+    </footer>
+)
