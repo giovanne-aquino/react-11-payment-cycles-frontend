2c0fa52bb31c11ec37fa1a68dfde9aa91aa0a0cd
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-27 12:21:56 -0300

    add css referencies and header.jsx

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,7 +1,12 @@
+import '../common/template/dependencies'
 import React from "react";
+import Header from '../common/template/header';
 
 export default props =>(
     <div className="wrapper">
-        <h1>App</h1>
+        <Header>
+        
+        </Header>
+      
     </div>
 )
