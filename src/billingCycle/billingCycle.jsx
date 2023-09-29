f2bdfe2d3ada12fdfb3f60dd6b96d414fb97fd5f
giovanne-aquino-clear <giovanneaquino15@gmail.com>
2023-09-29 10:25:05 -0300

    Update: connects billyngCycles to redux

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -6,9 +6,19 @@ import Tabs from '../common/tab/tabs'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/TabHeader'
+import TabContent from '../common/tab/TabContent'
+import { selectTab } from '../common/tab/tabActions'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { map } from 'lodash'
 
 
 class BillingCycle extends Component{
+
+    componentWillMount(){
+        this.props.selectTab('tabList')
+    }
+
     render(){
         return(
             <div>
@@ -24,6 +34,10 @@ class BillingCycle extends Component{
                        
                         </TabsHeader>
                         <TabsContent>
+                            <TabContent id= 'tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id= 'tabCreate'><h1>incluir</h1></TabContent>
+                            <TabContent id= 'tabUpdate'><h1>ALterar</h1></TabContent>
+                            <TabContent id= 'tabDelete'><h1>Excluir</h1></TabContent>
 
                         </TabsContent>
                     </Tabs>
@@ -32,6 +46,6 @@ class BillingCycle extends Component{
         )
     }
 }
-
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+export default connect(null,mapDispatchToProps)(BillingCycle )
 
