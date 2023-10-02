e01f91f4f0d99618a6aa5a9482e7f9d390c59f63
giovanne <giovanne.aquino.clear@gmail.com>
2023-10-02 09:29:12 -0300

    prepara projeto

--- webpack.config.js
+++ webpack.config.js
@@ -4,44 +4,44 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 module.exports = {
     entry: './src/index.jsx',
     output: {
-        path: __dirname + '/public',//define a pasta
-        filename: './app.js'// define a saida padrao(ou arquivo innicial)
+        path: __dirname + '/public',
+        filename: './app.js'
     },
-    devServer: {//define servidor
-        port: 8080,//define porta do servidor localhost
-        contentBase: './public',//define onde procurar
+    devServer: {
+        port: 8080,
+        contentBase: './public',
     },
     resolve: {
-        extensions: ['', '.js', '.jsx'],// define as extensões que interpreta
-        alias: {// cria "apelidos" (recebe valor como em uma variavel)
-            modules: __dirname + '/node_modules',// quando precisar basta referencar "modules"
-            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',//" " " " "jquery"
-            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'//" " " " " "bootstrap
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules',
+            jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+            bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
     },
-    plugins: [ //adiciona os plugins
+    plugins: [ 
         new webpack.ProvidePlugin({
-            $: 'jquery',// torna "jquery (definido na linha 18) disponivel como ' $ '"
-            jQuery: 'jquery',// $ = jQuery
+            $: 'jquery',
+            jQuery: 'jquery',
             'window.jQuery': 'jquery'
         }),
-        new ExtractTextPlugin('app.css')//adiciona classe css atraves do uploader
+        new ExtractTextPlugin('app.css')
     ],
     module: {
         loaders: [{
-            test: /.js[x]?$/,// deixa extensão padrao
-            loader: 'babel-loader',// loader
-            exclude: /node_modules/,// exclui
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
             query: {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
         }, {
             test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')//loader do css
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
-            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,//loader das demais extensões
-            loader: 'file'//loader usado para carregar os arquivos 
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            loader: 'file'
         }]
     }
 }
