dcf0fbf22ea1bde317158115741ef1db8fba5316
giovanne <giovanne.aquino.clear@gmail.com>
2023-10-02 12:15:52 -0300

    update: change app and  routes

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
-import App from './app'
+import App from './app.jsx'
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
+import AuthOrApp from './../auth/authOrApp'
 
 export default props => (
     <Router history={hashHistory}>
+        <Route path='/' component={AuthOrApp}></Route>
         <Route path='/' component={App}>
             <IndexRoute component={Dashboard} />
             <Route path='billingCycles' component={BillingCycle} />
